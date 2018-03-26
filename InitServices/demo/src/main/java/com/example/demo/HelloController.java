20d2d896182e3ccd3f56786c93503cbbf72d1c78
caoxin4813 <qzcx237@126.com>
2018-03-26 11:18:42 +0800

    Add HelloController.java

---
+++ InitServices/demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by caoxin on 2018/3/23.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
